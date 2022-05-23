2cc5a80a8b47a1771b8b3c87836cf07def91162b
Matt Mahowald <10021147+mattmahowald@users.noreply.github.com>
2022-05-23 22:31:03 +0000

    updates

--- cookies.js
+++ cookies.js
@@ -41,3 +41,163 @@ function getCookieScore() {
         return parseInt(score);
     }
 }
+
+function setCookieMultiplier(multiplier) {
+    setCookie("mathClickerMultiplier", multiplier, 7);
+}
+
+
+function getCookieMultiplier() {
+    let multiplier = getCookie("mathClickerMultiplier");
+    console.log(multiplier);
+    if(multiplier == "") {
+        setCookieMultiplier(1);
+        return 1;
+    } else {
+        return parseInt(multiplier);
+    }
+}
+
+function setCookieNumOne(numOne) {
+    setCookie("mathClickerNumOne", numOne, 7);
+}
+
+
+function getCookieNumOne() {
+    let numOne = getCookie("mathClickerNumOne");
+    console.log(numOne);
+    if(numOne == "") {
+        setCookieNumOne(0);
+        return 0;
+    } else {
+        return parseInt(numOne);
+    }
+}
+
+function setCookieNumTwo(numTwo) {
+    setCookie("mathClickerNumTwo", numTwo, 7);
+}
+
+
+function getCookieNumTwo() {
+    let numTwo = getCookie("mathClickerNumTwo");
+    console.log(numTwo);
+    if(numTwo == "") {
+        setCookieNumTwo(0);
+        return 0;
+    } else {
+        return parseInt(numTwo);
+    }
+}
+
+function setCookieNumSix(numSix) {
+    setCookie("mathClickerNumSix", numSix, 7);
+}
+
+
+function getCookieNumSix() {
+    let numSix = getCookie("mathClickerNumSix");
+    console.log(numSix);
+    if(numSix == "") {
+        setCookieNumSix(0);
+        return 0;
+    } else {
+        return parseInt(numSix);
+    }
+}
+
+function setCookieNumEight(numEight) {
+    setCookie("mathClickerNumEight", numEight, 7);
+}
+
+
+function getCookieNumEight() {
+    let numEight = getCookie("mathClickerNumEight");
+    console.log(numEight);
+    if(numEight == "") {
+        setCookieNumEight(0);
+        return 0;
+    } else {
+        return parseInt(numEight);
+    }
+}
+
+function setCookieThreeBought(threeBought) {
+    setCookie("mathClickerThreeBought", threeBought, 7);
+}
+
+
+function getCookieThreeBought() {
+    let threeBought = getCookie("mathClickerThreeBought");
+    console.log(threeBought);
+    if(threeBought == "" || threeBought == false) {
+        setCookieThreeBought(false);
+        return false;
+    } else {
+        return threeBought == "true";
+    }
+}
+
+function setCookieFourBought(fourBought) {
+    setCookie("mathClickerFourBought", fourBought, 7);
+}
+
+
+function getCookieFourBought() {
+    let fourBought = getCookie("mathClickerFourBought");
+    console.log(fourBought);
+    if(fourBought == "" || fourBought == false) {
+        setCookieFourBought(false);
+        return false;
+    } else {
+        return fourBought == "true";
+    }
+}
+
+function setCookiePicklesBought(picklesBought) {
+    setCookie("mathClickerPicklesBought", picklesBought, 7);
+}
+
+
+function getCookiePicklesBought() {
+    let picklesBought = getCookie("mathClickerPicklesBought");
+    console.log(picklesBought);
+    if(picklesBought == "" || picklesBought == false) {
+        setCookiePicklesBought(false);
+        return false;
+    } else {
+        return picklesBought == "true";
+    }
+}
+
+function setCookieSevenBought(sevenBought) {
+    setCookie("mathClickerSevenBought", sevenBought, 7);
+}
+
+
+function getCookieSevenBought() {
+    let sevenBought = getCookie("mathClickerSevenBought");
+    console.log(sevenBought);
+    if(sevenBought == "" || sevenBought == false) {
+        setCookieSevenBought(false);
+        return false;
+    } else {
+        return sevenBought == "true";
+    }
+}
+
+function setCookieNineBought(nineBought) {
+    setCookie("mathClickerNineBought", nineBought, 7);
+}
+
+
+function getCookieNineBought() {
+    let nineBought = getCookie("mathClickerNineBought");
+    console.log(nineBought);
+    if(nineBought == "" || nineBought == false) {
+        setCookieNineBought(false);
+        return false;
+    } else {
+        return nineBought == "true";
+    }
+}
--- game.js
+++ game.js
@@ -1,5 +1,8 @@
 // State variables that control our program
+var BALL_RADIUS = 40;
+
 var ball;
+var state;
 
 var titleText;
 var newText;
@@ -10,6 +13,7 @@ var multiplier;
 var atShop;
 var atBall;
 var txt;
+var pickles;
 
 var oneCost;
 var twoCost;
@@ -49,15 +53,54 @@ var shopText;
 var homeText;
 var rectColor;
 
+var saveText;
+
+var random;
+
+var IMAGES = [
+  "assets/antzcircle.png",
+  "assets/bee.png",
+  "assets/deku.png",
+  "assets/mice.png",
+  "assets/mort.png",
+  "assets/ninja.png",
+  "assets/oth.png",
+  "assets/oth2.png",
+  "assets/pickle.png",
+  "assets/pib.png",
+  "assets/saiki.png",
+  "assets/shrek.png",
+  "assets/sinbad.png",
+  "assets/tanaka.png",
+  "assets/will.png",
+  "assets/pollo.png"
+]
+
 //it indeed updates the score
 function updateScore(newScore) {
+    if(newScore != score) {
+      setCookieScore(newScore);
+    }
     score = newScore;
-    setCookieScore(newScore);
+
+}
+
+function drawImage() {
+    var ballRadius = ball.getRadius();
+    remove(ball);
+    var randomIndex = Randomizer.nextInt(0, IMAGES.length - 1)
+    ball = new WebImage(IMAGES[randomIndex]);
+    ball.setSize(ballRadius*2, ballRadius*2);
+    ball.setPosition(
+      (getWidth() - ball.getWidth())/2,
+      (getHeight() - ball.getHeight()) / 2
+    );
+    add(ball);
 }
 
 //when you clicc you get point
 function click(e) {
-    elem = getElementAt(e.getX(), e.getY());
+    elem = getElementAt(e.getX(), e.getY())
     if(atBall == true && elem == ball) {
         ball.setColor(Randomizer.nextColor());
         updateScore(score + 1 * multiplier);
@@ -67,9 +110,20 @@ function click(e) {
             150
         );
         clickSound.play();
+        random = Randomizer.nextInt(1, 100);
+        if(ball.type == "WebImage"){
+            remove(ball);
+            console.log("webImage");
+            ball = new Circle(BALL_RADIUS);
+            ball.setPosition(getWidth()/2, getHeight()/2);
+            ball.setColor(Randomizer.nextColor());
+            add(ball);
+        }
+        if(random == 100){
+            drawImage();
+        }
     }
     
-    console.log(elem);
     if(elem == shopRect || elem == shopText){
         goToShop();
     }
@@ -92,14 +146,16 @@ function click(e) {
         twoCost = 200;
         numSix = 0;
         numEight = 0;
-        sixCost = 4000;
+        sixCost = 2000;
         eightCost = 8000;
         threeBought = false;
         fourBought = false;
         picklesBought = false;
         sevenBought = false;
         nineBought = false;
+        add(saveText);
         atBall = true;
+        lobbySong.play();
     }
     if(elem == continueText){
         remove(newText);
@@ -110,8 +166,58 @@ function click(e) {
         add(shopText);
         add(homeRect);
         add(homeText);
+        score = getCookieScore();
+        multiplier = getCookieMultiplier();
+        numOne = getCookieNumOne();
+        numTwo = getCookieNumTwo();
+        numSix = getCookieNumSix();
+        numEight = getCookieNumEight();
+        oneCost = 10 * Math.pow(2, numOne);
+        twoCost = 200 * Math.pow(2, numTwo);
+        sixCost = 2000 * Math.pow(2, numSix);
+        eightCost = 10000 * Math.pow(2, numEight);
+        threeBought = getCookieThreeBought();
+        fourBought = getCookieFourBought();
+        picklesBought = getCookiePicklesBought();
+        sevenBought = getCookieSevenBought();
+        nineBought = getCookieNineBought();
+        items["autoClicker"].setText("Auto Clicker $" + oneCost);
+        items["superClicker"].setText("Super Clicker $" + twoCost);
+        if(threeBought == false){
+            items["doubleClicks"].setText("Double Clicks $500");
+        }else{
+            items["doubleClicks"].setText("SOLD OUT!");
+        }
+        if(fourBought == false){
+            items["tripleClicks"].setText("Triple Clicks $1000");
+        }else{
+            items["tripleClicks"].setText("SOLD OUT!");
+        }
+        items["ultraClicker"].setText("Ultra Clicker $" + sixCost);
+        if(sevenBought == false){
+            items["quintClicks"].setText("Quint Clicks $4000");
+        }else{
+            items["quintClicks"].setText("SOLD OUT!");
+        }
+        items["maxClicker"].setText("Max Clicker $" + eightCost);
+        if(nineBought == false){
+            items["maxClicks"].setText("Max Clicks $10000");
+        }else{
+            items["maxClicks"].setText("SOLD OUT!");
+        }
+        if(picklesBought == false){
+            items["pickles"].setText("Pickles $1000000")
+        }else{
+          pickles.setPosition(getWidth()/2+getWidth()/4, getHeight()/2);
+          pickles.setSize(100, 100);
+            add(pickles);
+            items["pickles"].setText("SOLD OUT!");
+        }
+        add(saveText);
         atBall = true;
+        lobbySong.play();
     }
+    
     if(elem == items.autoClicker && atShop == true && score >= oneCost){
         solveMultiplication();
         updateScore(score-oneCost);
@@ -143,7 +249,6 @@ function click(e) {
     if(elem == items.pickles && atShop == true && score >= 1000000 && !picklesBought){
         solveMultiplication();
         updateScore(score-1000000);
-        var pickles = new WebImage("https://images.heb.com/is/image/HEBGrocery/000151575?fit=constrain,1&wid=800&hei=800&fmt=jpg&qlt=85,0&resMode=sharp2&op_usm=1.75,0.3,2,0");
         pickles.setSize(100, 100);
         pickles.setPosition(getWidth()/2 + getWidth()/4, getWidth()/2);
         add(pickles);
@@ -171,22 +276,38 @@ function click(e) {
         eightCost = eightCost * 2;
         items.maxClicker.setText("Max Clicker $" + eightCost);
     }
-    if(elem == maxClicks && atShop == true && score >= 10000 && !nineBought){
+    
+    if(elem == items.maxClicks && atShop == true && score >= 10000 && !nineBought){
         solveFractionAddition();
         updateScore(score-10000);
         multiplier=multiplier*10;
         items.maxClicks.setText("SOLD OUT!");
         nineBought = true;
     }
+    if(elem == saveText){
+        println("saving");
+        setCookieScore(score);
+        setCookieMultiplier(multiplier);
+        setCookieNumOne(numOne);
+        setCookieNumTwo(numTwo);
+        setCookieNumSix(numSix);
+        setCookieNumEight(numEight);
+        setCookieThreeBought(threeBought);
+        setCookieFourBought(fourBought);
+        setCookiePicklesBought(picklesBought);
+        setCookieSevenBought(sevenBought);
+        setCookieNineBought(nineBought);
+    }
 }
 
+var hoverElem = null;
 function hover(e){
-    if(elem.type == 'Text') {
-        elem.setColor(Color.BLACK);
+    if(hoverElem != null && hoverElem.type == 'Text') {
+        hoverElem.setColor(Color.BLACK);
     }
-    elem = getElementAt(e.getX(), e.getY());
-    if(elem.type == 'Text' && elem != txt && elem !=answerTxt && elem != questionTxt && elem != titleText) {
-        elem.setColor(Color.CYAN);
+    hoverElem = getElementAt(e.getX(), e.getY());
+    if(hoverElem.type == 'Text' && hoverElem != txt && hoverElem !=answerTxt && hoverElem != questionTxt && hoverElem != titleText) {
+        hoverElem.setColor(Color.CYAN);
     }
 }
 
@@ -207,14 +328,15 @@ function removeItemsFromCanvas() {
 }
 function goToBall(){
     if(atShop == true){
-    removeItemsFromCanvas();
-    add(ball);
-    add(txt);
-    atShop = false;
-    atBall = true;
-    shopSong.pause();
-    lobbySong.play();
-    lobbySong.loop = true;
+        removeItemsFromCanvas();
+        add(ball);
+        add(txt);
+        atShop = false;
+        atBall = true;
+        shopSong.pause();
+        lobbySong.play();
+        lobbySong.loop = true;
+        add(saveText);
     }
 }
 //controls go to shop controls and screen goes to shop screen
@@ -231,6 +353,7 @@ function goToShop(e) {
     add(shopText);
     add(homeRect);
     add(homeText);
+    remove(saveText);
 }
 
 //these numbered variables are the autoclickers
@@ -243,7 +366,7 @@ function one(){
     );
 }
 function two(){
-    updateScore(score + 4 * numTwo);
+    updateScore(score + 3 * numTwo);
     txt.setText("Money: " + score);
     txt.setPosition(
         (getWidth() - txt.getWidth()) / 2,
@@ -251,7 +374,7 @@ function two(){
     );
 }
 function six(){
-    updateScore(score + 30 * numSix);
+    updateScore(score + 10 * numSix);
     txt.setText("Money: " + score);
     txt.setPosition(
         (getWidth() - txt.getWidth()) / 2,
@@ -259,7 +382,7 @@ function six(){
     );
 }
 function eight(){
-    updateScore(score + 100 * numEight);
+    updateScore(score + 20 * numEight);
     txt.setText("Money: " + score);
     txt.setPosition(
         (getWidth() - txt.getWidth()) / 2,
@@ -370,10 +493,42 @@ function title(){
     add(continueText);
 }
 
+function pulse(){
+    if(ball.type == "WebImage") {
+        if(state == 0){
+            ball.setSize(ball.getWidth()+2, ball.getHeight()+2);
+            if(ball.getWidth() > 90){
+                state = 1;
+            }
+        }else{
+            ball.setSize(ball.getWidth()-2, ball.getHeight()-2);
+            if(ball.getWidth() < 70){
+                state = 0;
+            }
+        }
+        ball.setPosition(getWidth()/2 - ball.getWidth()/2, getHeight()/2 - ball.getWidth()/2);
+    } else {
+        if(state == 0){
+            ball.setRadius(ball.getRadius()+1);
+            if(ball.getRadius() > 45){
+                state = 1;
+            }
+        }else{
+            ball.setRadius(ball.getRadius()-1);
+            if(ball.getRadius() < 35){
+                state = 0;
+            }
+        }
+    }
+}
+
+
 //sets stuff up
 function start() {
-    ball = new Circle(40); // WebImage("assets/antzcircle.png");
+    ball = new Circle(BALL_RADIUS); // WebImage("assets/antzcircle.png");
     add(ball);
+    state = 0;
+    setTimer(pulse, 100);
     title();
     rectColor = new Color(150, 150, 150)
     shopRect = new Rectangle(getWidth()/2, 50);
@@ -386,13 +541,12 @@ function start() {
     shopText.setPosition(getWidth()/8, 40);
     homeText = new Text("Home", "30pt Arial");
     homeText.setPosition(getWidth()/2 + getWidth()/8, 40);
+    pickles = new WebImage("https://images.heb.com/is/image/HEBGrocery/000151575?fit=constrain,1&wid=800&hei=800&fmt=jpg&qlt=85,0&resMode=sharp2&op_usm=1.75,0.3,2,0");
     lobbySong = new Audio("https://codehs.com/uploads/377139db115f3698f4fe39d4dcd5640b");
-    lobbySong.play();
     lobbySong.loop = true;
     shopSong = new Audio("https://codehs.com/uploads/439f154704a802e7e9aa11baf07a555b");
     chaChing = new Audio("https://codehs.com/uploads/43ddfb3a7869ca80bc4b841e098f4375");
     clickSound = new Audio("https://codehs.com/uploads/909b1a775659da951e33f5c5f05716e5")
-    //change to score = getCookieScore for saving
     score = 0;
     multiplier = 1;
     atShop = false;
@@ -405,17 +559,14 @@ function start() {
     twoCost = 200;
     sixCost = 2000;
     eightCost = 8000;
-    threeBought = false;
-    fourBought = false;
-    picklesBought = false;
-    sevenBought = false;
-    nineBought = false;
+    saveText = new Text("Save", "30pt Arial");
+    saveText.setPosition(getWidth()/2 - saveText.getWidth()/2, getHeight()/2 + getHeight()/4);
     mouseMoveMethod(hover);
     items["autoClicker"] = new Text("Auto Clicker $" + oneCost, "30pt Arial");
     items["superClicker"] = new Text("Super Clicker $" + twoCost, "30pt Arial");
     items["doubleClicks"] = new Text("Double Clicks $500", "30pt Arial");
     items["tripleClicks"] = new Text("Triple Clicks $1000", "30pt Arial");
-    items["pickles"] = new Text("Pickles $100000", "30pt Arial");
+    items["pickles"] = new Text("Pickles $1000000", "30pt Arial");
     items["ultraClicker"] = new Text("Ultra Clicker $" + sixCost, "30pt Arial");
     items["quintClicks"] = new Text("Quint Clicks $4000", "30pt Arial");
     items["maxClicker"] = new Text("Max Clicker $" + eightCost, "30pt Arial");
@@ -438,8 +589,8 @@ function start() {
     mouseClickMethod(click);
     txt = new Text("Money: " + score, "30pt Arial")
     txt.setPosition(120, 150);
-    setTimer(one, 200);
-    setTimer(two, 200);
-    setTimer(six, 200);
+    setTimer(one, 1000);
+    setTimer(two, 500);
+    setTimer(six, 350);
     setTimer(eight, 200);
 }
